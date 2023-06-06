03f9984395ee6bdce313d1b9d838208beac6b860
Jon Granados <grxnxdos@gmail.com>
2023-06-06 15:12:36 -0700

    feat: render ImageDetailPage's content

--- src/components/ImageDetailsPage.jsx
+++ src/components/ImageDetailsPage.jsx
@@ -1,6 +1,22 @@
 export function ImageDetailsPage({ selectedArt, setSelectedArt }) {
+	const artist = selectedArt.artist_title ?? 'Unknown Artist';
+	const title = selectedArt.title;
+	const altText = selectedArt.thumbnail.alt_text;
+	const imageId = selectedArt.image_id;
+	const imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
+
+	console.log(selectedArt);
 	function buttonClickHandler() {
-		setSelectedArt(false);
+		setSelectedArt();
 	}
-	return <button onClick={() => buttonClickHandler()}>Back to Results</button>;
+	return (
+		<>
+			<div>
+				<h1>{title}</h1>
+				<h2>{`by ${artist}`}</h2>
+				<button onClick={() => buttonClickHandler()}>Back to Results</button>
+			</div>
+			<img alt={altText} src={imageUrl} />
+		</>
+	);
 }
